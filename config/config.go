cf88a05b7947bce7b41edd8160b221bf64e31798
Alec Burton <alecburton90@gmail.com>
2017-10-15 15:21:03 +0100

    Add more comments

--- config/config.go
+++ config/config.go
@@ -6,11 +6,13 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
+// Represents database server
 type Config struct {
 	Server   string
 	Database string
 }
 
+// Read and parse a configuration file "config.toml"
 func (c *Config) Read() {
 	if _, err := toml.DecodeFile("config.toml", &c); err != nil {
 		log.Fatal(err)
--- dao/pets_dao.go
+++ dao/pets_dao.go
@@ -21,7 +21,8 @@ const (
 	COLLECTION = "pets"
 )
 
-func (p *PetsDAO) connect() {
+// Establish a connection to DB
+func (p *PetsDAO) Connect() {
 	session, err := mgo.Dial(p.Server)
 	if err != nil {
 		log.Fatal(err)
@@ -29,28 +30,33 @@ func (p *PetsDAO) connect() {
 	db = session.DB(p.Database)
 }
 
+// Get a list of all pets
 func (p *PetsDAO) FindAll() ([]Pet, error) {
 	var pets []Pet
 	err := db.C(COLLECTION).Find(bson.M{}).All(&pets)
 	return pets, err
 }
 
+// find a pet by ID
 func (p *PetsDAO) FindById(id string) (Pet, error) {
 	var pet Pet
 	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&pet)
 	return pet, err
 }
 
+// insert a pet into the DB
 func (p *PetsDAO) Insert(pet Pet) error {
 	err := db.C(COLLECTION).Insert(&pet)
 	return err
 }
 
+// delete an existing pet
 func (p *PetsDAO) Delete(pet Pet) error {
 	err := db.C(COLLECTION).Remove(&pet)
 	return err
 }
 
+// update an existing pet
 func (p *PetsDAO) Update(pet Pet) error {
 	err := db.C(COLLECTION).UpdateId(pet.ID, &pet)
 	return err
